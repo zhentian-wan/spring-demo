464f2c47b4534e306669fc19c188f8923b6dbce3
zhentian-wan <answer881215@gmail.com>
2018-08-13 21:25:08 +0300

    Using applcationContext and Setter Injection

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,11 +1,17 @@
 package com.pluralsight;
 
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main (String[] args) {
-        CustomerService service = new CustomerServiceImpl();
+       // CustomerService service = new CustomerServiceImpl();
+
+        // Find the applicationContext.xml file
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // Using application context to replace hardcodedCustomerServiceImpl
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
         System.out.println(service.findAll().get(0).getFirstname());
     }
 }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,13 +2,19 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+
+    //private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
 
     @Override
     public List<Customer> findAll() {
